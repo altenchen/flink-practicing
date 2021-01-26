1be43688b338f23e7c0d86211dbb549a152266b7
chenchen <chenchen@bitnei.cn>
2021-01-26 18:16:08 +0800

    -[add]JavaBasic, callback;

---
+++ src/main/java/javabasic/callback/A.java
@@ -0,0 +1,25 @@
+package javabasic.callback;
+
+/**
+ * @author altenchen
+ * @time 2021/1/26
+ * @description 功能
+ */
+public class A {
+    
+    public static void main(String[] args) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                new B().handle(
+                        "handle something",
+                        data-> {
+                            System.out.println(getClass().getName() + "系统的回调方法，收到数据：" + data);
+                        });
+            }
+        }).start();
+        System.out.println("A系统异步回调，先做其他事情");
+    }
+    
+    
+}
---
+++ src/main/java/javabasic/callback/Callback.java
@@ -0,0 +1,12 @@
+package javabasic.callback;
+
+/**
+ * @author altenchen
+ * @time 2021/1/26
+ * @description 功能
+ */
+public interface Callback {
+    
+    void onResponse(String data);
+    
+}
