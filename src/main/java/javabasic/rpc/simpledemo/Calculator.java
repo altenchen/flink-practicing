1b6201e742f1b001bb833e0e4cc7cb0aba1d4933
chenchen <chenchen@bitnei.cn>
2021-02-23 15:13:47 +0800

    -[add]JavaBasic, rpc简单实现;

---
+++ src/main/java/javabasic/rpc/simpledemo/Calculator.java
@@ -0,0 +1,10 @@
+package javabasic.rpc.simpledemo;
+
+/**
+ * @author altenchen
+ * @time 2020/12/21
+ * @description 功能
+ */
+public interface Calculator {
+    int add(int a, int b);
+}
---
+++ src/main/java/javabasic/rpc/simpledemo/CalculatorRemoteImpl.java
@@ -0,0 +1,14 @@
+package javabasic.rpc.simpledemo;
+
+/**
+ * @author altenchen
+ * @time 2020/12/21
+ * @description 功能
+ */
+//todo
+public class CalculatorRemoteImpl implements Calculator {
+    @Override
+    public int add(int a, int b) {
+        return 0;
+    }
+}
