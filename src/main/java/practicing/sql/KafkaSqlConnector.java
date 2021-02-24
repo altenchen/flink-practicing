e9b0bac0777d167b617441c5fcc0c8c137de7595
chenchen <chenchen@bitnei.cn>
2021-02-24 18:02:53 +0800

    -[add]Flink两阶段提交，EXACTLY_ONCE语义，kafka2Mysql实践；

--- src/main/java/practicing/sql/KafkaSqlConnector.java
+++ src/main/java/practicing/sql/KafkaSqlConnector.java
@@ -1,4 +1,4 @@
-package self.practice.sql;
+package practicing.sql;
 
 /**
  * @author altenchen
---
+++ src/main/java/practicing/twophasecommit/Demo.java
@@ -0,0 +1,9 @@
+package practicing.twophasecommit;
+
+/**
+ * @author altenchen
+ * @time 2021/2/24
+ * @description 功能
+ */
+public class Demo {
+}
